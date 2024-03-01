7bc85d26a416a53a226806874ee8d15838400ce6
priyanshi singh <8115700261aman@gmail.com>
2024-03-02 00:05:29 +0530

    02-03-24

---
+++ C_Find_a_Mine.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef unsigned long long ull;
+
+void solve()
+{
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--) solve();
+    return 0;
+}
